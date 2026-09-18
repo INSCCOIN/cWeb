3e0b654a2d5632c312a215da905563a3320f9ba3
Ian Simpson <53959046+INSCCOIN@users.noreply.github.com>
2026-09-18 00:01:18 -0400

    Add HTML header file with structures and functions

---
+++ html.h
@@ -0,0 +1,31 @@
+#ifndef HTML_H
+#define HTML_H
+#include <stddef.h>
+
+enum { MAX_LINKS = 64, MAX_HREF = 256, MAX_TEXT = 96, MAX_IMG = 8, IMG_W = 48, IMG_H = 18 };
+
+typedef struct {
+    char href[MAX_HREF];
+    char text[MAX_TEXT];
+} Link;
+
+typedef struct {
+    char href[MAX_HREF];
+    char alt[MAX_TEXT];
+    unsigned char pix[IMG_W * IMG_H];
+    int ok;
+} Img;
+
+typedef struct {
+    char *body;
+    Link link[MAX_LINKS];
+    Img img[MAX_IMG];
+    int nlink, nimg;
+    char title[80];
+} Page;
+
+void page_free(Page *p);
+int html_parse(const char *html, Page *out);
+void url_join(char *dst, size_t n, const char *base, const char *rel);
+
+#endif
